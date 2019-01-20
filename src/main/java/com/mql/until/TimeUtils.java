7c49ea57f568c57f737e6bd334247c8e60ecf963
lkkkGitHub <963087276@qq.com>
2019-01-20 18:24:54 +0800

    TimeUtils 增加作者注释，删除没有用的方法

--- src/main/java/com/mql/until/TimeUtils.java
+++ src/main/java/com/mql/until/TimeUtils.java
@@ -2,20 +2,13 @@ package com.mql.until;
 
 import java.sql.Time;
 import java.text.DateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * @author mql
+ * @date 2018/12/25
+ */
 public class TimeUtils {
-
-    /**
-     * 获取当前时间
-     *
-     * @return Date类型时间
-     */
-    public static Date getNowTime() {
-        Calendar calendar = Calendar.getInstance();
-        return calendar.getTime();
-    }
 
     /**
      * 获取java.sql.Timestamp类型的当前时间
@@ -24,29 +17,6 @@ public class TimeUtils {
      */
     public static java.sql.Timestamp getNowTimestamp() {
         return new java.sql.Timestamp((new Date()).getTime());
-    }
-
-    /**
-     * 在当前时间基础上加 integer 个 分钟数值
-     *
-     * @param integer
-     * @return
-     */
-    public static Date addMinuteTime(Integer integer) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MINUTE, integer);
-        return calendar.getTime();
-    }
-
-    /**
-     * 比较now是否在future之前
-     *
-     * @param now
-     * @param future
-     * @return
-     */
-    public static Boolean compareTime(Date now, Date future) {
-        return now.before(future);
     }
 
     /**
